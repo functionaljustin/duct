8b751c3bf127733b6cd9d02ae5d5f18c8673672d
Justin Heyes-Jones <justinhj@gmail.com>
2022-06-20 16:50:26 -0700

    imported

--- examples/src/main/scala/examples/video12.scala
+++ examples/src/main/scala/examples/video12.scala
@@ -1,24 +1,9 @@
+
 object Video12 extends App:
 
   import org.justinhj.duct.typeclasses.monad.{eitherMonad, _}
   import org.justinhj.duct.typeclasses.numeric.{given, _}
-
-  case class ReaderT[F[_],R,A](run: R => F[A])
-
-  object ReaderT:
-    def lift[F[_],R,A](fa: F[A]): ReaderT[F,R,A] = ReaderT(_ => fa)
-
-  given readerTMonad[F[_]: Monad,R]: Monad[[A1] =>> ReaderT[F,R,A1]] with
-    def pure[A](a:A): ReaderT[F,R,A] = ReaderT(_ => Monad[F].pure(a))
-
-    extension [A,B](far: ReaderT[F,R,A])
-      def flatMap(f: A => ReaderT[F,R,B]): ReaderT[F,R,B] = {
-        ReaderT((r: R) =>
-          val fa: F[A] = far.run(r)
-          val fb: F[B] = fa.flatMap(b => f(b).run(r))
-          fb
-        )
-      }
+  import org.justinhj.duct.datatypes.{given, _}
 
   import Exp._
   import Video12._
