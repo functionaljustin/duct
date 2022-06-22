356aae2227230140bb489035bc26f8384faad2b5
Justin Heyes-Jones <justinhj@gmail.com>
2022-06-21 23:03:07 -0700

    nel supreme

--- core/src/main/scala/org/justinhj/duct/datatypes/NonEmptyList.scala
+++ core/src/main/scala/org/justinhj/duct/datatypes/NonEmptyList.scala
@@ -1,16 +1,15 @@
 package org.justinhj.duct.datatypes
 
-case class NonEmptyList[A](head:A, tail:A*) {
-  def append(right: NonEmptyList[A]): NonEmptyList[A] = {
+case class NonEmptyList[+A](head:A, tail:A*):
+  // List we're appending to must be type A or a super-type
+  def append[B >: A](right: NonEmptyList[B]): NonEmptyList[B] =
     NonEmptyList(head, (tail :+ right.head) ++ right.tail:_*)
-  }
+
   def toList: List[A] = (head +: tail).toList
-}
 
 object NonEmptyList:
-  def fromSeq[A](items: Seq[A]): Option[NonEmptyList[A]] = {
+  def fromSeq[A](items: Seq[A]): Option[NonEmptyList[A]] =
     if items.size > 0 then
       Some(NonEmptyList(items.head, items.tail:_*))
     else
       None
-  }
--- core/src/test/scala/org/justinhj/duct/NonEmptyListTests.scala
+++ core/src/test/scala/org/justinhj/duct/NonEmptyListTests.scala
@@ -1,13 +1,14 @@
 import org.justinhj.duct.datatypes.NonEmptyList
 
 class NonEmptyListSuite extends munit.FunSuite {
-  test("create lists of various arities") {
+  test("create and append nonemptylists") {
     val oneList = NonEmptyList(1)
     val twoList = NonEmptyList(2,3)
     val threeList = NonEmptyList("four", "five", "six")
 
     val onetwoList = oneList.append(twoList)
-    // val onetwothreeList = onetwoList.append(threeList)
+    val onetwothreeList = onetwoList.append(threeList)
     assertEquals(onetwoList.toList, List(1,2,3))
+    assertEquals(onetwothreeList.toList, List(1,2,3,"four","five","six"))
   }
 }
