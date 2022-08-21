24acb0652aa3c9b67d2bdb1b952bb05bc25c73a4
Justin Heyes-Jones <justinhj@gmail.com>
2022-08-21 00:16:38 -0700

    lazy list experiment

--- core/src/main/scala/org/justinhj/duct/datatypes/LazyList.scala
+++
@@ -1,6 +0,0 @@
-package org.justinhj.duct.datatypes
-
-case class LazyList[A](val head: Option[() => A], val tail: Option[LazyList[A]]) {
-        lazy val first: A = head()
-        lazy val rest = tail(first)
-}
--- examples/src/main/scala/examples/LazyList.scala
+++ examples/src/main/scala/examples/LazyList.scala
@@ -1,5 +1,62 @@
 package examples
-import org.justinhj.duct.datatypes.LazyList
 
 object LazyListDemo extends App:
-  println("Hello!")
+  // example of lazy evaluation
+  class LazyThing[A](a: => A) {
+    lazy val get = a
+  }
+
+  val f1 = {
+    println("eval 10")
+    10
+  }
+
+  val lazyThing1 = LazyThing(f1)
+
+  println(lazyThing1.get)
+  println(lazyThing1.get)
+
+  object OurLazyList:
+    def fromSeq[A](in: Seq[A]): OurLazyList[A] = {
+      in match {
+        case first :: rest => 
+          OurLazyList(Some(() => first), Some(() => fromSeq(rest)))
+        case Nil => OurLazyList(None,None)
+      }
+    }
+
+  // Implementing a lazy list
+  class OurLazyList[A](head: Option[() => A], tail: Option[() => OurLazyList[A]]) {
+    lazy val getHead: Option[A] = head.map(f => f())
+    lazy val getTail: Option[OurLazyList[A]] = tail.map(f => f())
+
+    def forEach(f: A => Unit): Unit = {
+      def forEachHelper(f: A => Unit, in: OurLazyList[A]): Unit = {
+        in.getHead match {
+          case Some(a) => {
+            f(a)
+            in.getTail match {
+              case Some(tail) => 
+                forEachHelper(f,tail)
+              case None =>
+                None
+            }
+          }
+          case None => ()
+        }
+      }
+      forEachHelper(f, this)
+    }
+  }
+
+  def evalNum(n: Int) = {
+    println(s"evaluating $n")
+    n
+  }
+
+  val l1 = List(1,2,3).map(evalNum)
+  val lazyL1 = OurLazyList.fromSeq(l1)
+  lazyL1.forEach(a => 
+      println(a))
+  lazyL1.forEach(a => 
+      println(a))
