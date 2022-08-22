78c47872d8f68d6494cbcb126dd4efe6616abe01
Justin Heyes-Jones <justinhj@gmail.com>
2022-08-21 18:17:54 -0700

    implement lazy list map

--- examples/src/main/scala/examples/LazyList.scala
+++ examples/src/main/scala/examples/LazyList.scala
@@ -6,7 +6,7 @@ object LazyListDemo extends App:
     lazy val get = a
   }
 
-  val f1 = {
+  def f1 = {
     println("eval 10")
     10
   }
@@ -19,31 +19,36 @@ object LazyListDemo extends App:
   object OurLazyList:
     def fromSeq[A](in: Seq[A]): OurLazyList[A] = {
       in match {
-        case first :: rest => 
+        case first :: rest =>
           OurLazyList(Some(() => first), Some(() => fromSeq(rest)))
-        case Nil => OurLazyList(None,None)
+        case Nil => OurLazyList(None, None)
       }
     }
 
   // Implementing a lazy list
-  class OurLazyList[A](head: Option[() => A], tail: Option[() => OurLazyList[A]]) {
+  class OurLazyList[A](
+      head: Option[() => A],
+      tail: Option[() => OurLazyList[A]]
+  ) {
     lazy val getHead: Option[A] = head.map(f => f())
     lazy val getTail: Option[OurLazyList[A]] = tail.map(f => f())
 
+    def map[B](f: A => B): OurLazyList[B] = {
+      head match {
+        case Some(hd) => 
+          OurLazyList(Some(() => f(getHead.get)), 
+            getTail.map(ta => () => ta.map(f)))
+        case None =>
+          OurLazyList(None,None)
+      }
+    }
+
     def forEach(f: A => Unit): Unit = {
       def forEachHelper(f: A => Unit, in: OurLazyList[A]): Unit = {
-        in.getHead match {
-          case Some(a) => {
-            f(a)
-            in.getTail match {
-              case Some(tail) => 
-                forEachHelper(f,tail)
-              case None =>
-                None
-            }
-          }
-          case None => ()
-        }
+        in.getHead.foreach(a =>
+          f(a)
+          in.getTail.foreach(tail =>
+              forEachHelper(f, tail)))
       }
       forEachHelper(f, this)
     }
@@ -54,9 +59,9 @@ object LazyListDemo extends App:
     n
   }
 
-  val l1 = List(1,2,3).map(evalNum)
+  val l1 = List(1, 2, 3).map(evalNum)
   val lazyL1 = OurLazyList.fromSeq(l1)
-  lazyL1.forEach(a => 
-      println(a))
-  lazyL1.forEach(a => 
-      println(a))
+  val lazyL2 = lazyL1.map(_ + 1)
+
+  // lazyL1.forEach(a => println(a))
+  lazyL2.forEach(a => println(a))
